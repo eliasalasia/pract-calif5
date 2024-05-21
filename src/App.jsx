2a4e94371ab071c24209130e15b718d136140364
ELIAS <elias.alasia.moreno@gmail.com>
2024-05-21 16:12:09 -0500

    guardar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import {Header} from './components/Header'
 import { Main } from './components/Main'
 import { Services } from './components/Services'
+import { TestimonialContainer } from './components/TestimonialContainer'
+import { Gallery} from './components/Gallery'
+import { Footer } from './components/Footer'
+
 function App() {
   
 
@@ -9,6 +13,9 @@ function App() {
       <Header/>
       <Main/>
       <Services/>
+      <TestimonialContainer/>
+      <Gallery/>
+      <Footer/>
     </main>
   )
 }
